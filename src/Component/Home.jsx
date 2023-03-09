71a582efcb3429750340d88ad975dc19f105c287
prajwalda <52370665+prajwalda@users.noreply.github.com>
2023-03-09 19:51:32 +0530

    commit

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,61 +1,67 @@
-import React, { useState } from 'react'
+import { Heading } from '@chakra-ui/react'
+import { Button } from '@chakra-ui/react'
+import React from 'react'
+import { useState } from 'react'
 import "../Component/Home.scss"
 
 const Home = () => {
 
-  const [answer, setAnswer] = useState("");
+  const [answer,setAnswer] = useState("")
+  const [text,setText] = useState("")
+  
+  const clickHandler = (e) => {
+    setAnswer(answer + e.target.value)
 
-
-  const handleClick = (e) => {
-    setAnswer(answer.concat(e.target.value))
   }
 
-  const clearbtn = () =>{
+  const clearall = () => {
+    
     setAnswer("")
+
   }
 
-  const evaluate = () =>{
+  const delhandle = () => {
+    setAnswer(answer.slice(0,answer.length-1))
+    
+  }
+
+  const evaluate = () => {
     setAnswer(eval(answer))
   }
 
-  return (
-    <div className='home'>
-      <h2>Calculator Made with React js</h2>
 
-      <input type="text" readOnly value={answer}/>
-      <div className='box'>
-          <div>
-            <button className="btn" value={1} onClick={handleClick}>1</button>
-            <button className="btn" value={2} onClick={handleClick}>2</button>
-            <button className="btn" value={3} onClick={handleClick}>3</button>
-            <button className="btn" value={"+"} onClick={handleClick}>+</button>
-          </div>
+  return <div>
+    <h1>CALCULATOR</h1>
 
-          <div>
-            <button className="btn" value={4} onClick={handleClick}>4</button>
-            <button className="btn" value={5} onClick={handleClick}>5</button>
-            <button className="btn" value={6} onClick={handleClick}>6</button>
-            <button className="btn" value={"-"} onClick={handleClick}>-</button>
-          </div>
+    <form>
+      
+      <div className="box">
+        <input type="text" className='output' value={answer} readOnly/>
+        <input type="button" value="AC" className='span-two' onClick={clearall}/>
+        <input type="button" value="DEL" onClick={delhandle}/>
+        <input type="button" value="-" onClick={clickHandler}/>
+        <input type="button" value="1" onClick={clickHandler}/>
+        <input type="button" value="2" onClick={clickHandler}/>
+        <input type="button" value="3" onClick={clickHandler}/>
+        <input type="button" value="+" onClick={clickHandler}/>
+        <input type="button" value="4" onClick={clickHandler}/>
+        <input type="button" value="5" onClick={clickHandler}/>
+        <input type="button" value="6" onClick={clickHandler}/>
+        <input type="button" value="*" onClick={clickHandler}/>
+        <input type="button" value="7" onClick={clickHandler}/>
+        <input type="button" value="8" onClick={clickHandler}/>
+        <input type="button" value="9" onClick={clickHandler}/>
+        <input type="button" value="/" onClick={clickHandler}/>
+        <input type="button" value="," onClick={clickHandler}/>
+        <input type="button" value="0" onClick={clickHandler}/>
+        <input type="button" value="="  className='span-two' onClick={evaluate}/>
 
-          <div>
-            <button className="btn" value={7} onClick={handleClick}>7</button>
-            <button className="btn" value={8} onClick={handleClick}>8</button>
-            <button className="btn" value={9} onClick={handleClick}>9</button>
-            <button className="btn" value={"*"} onClick={handleClick}>*</button>
-          </div>
-          
-          <div>
-            <button className="btn" value={"%"} onClick={handleClick}>%</button>
-            <button className="btn">M+</button>
-            <button className="btn" onClick={clearbtn}>Clr</button>
-            <button className="btn" onClickCapture={evaluate}>=</button>
-          </div>
       </div>
-    </div>
 
+     
+    </form>
 
-  )
+  </div>
 }
 
 export default Home
